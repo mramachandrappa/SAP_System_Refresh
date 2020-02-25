15fe95d11c811664d50601e385aabbc684122269
root <root@ip-172-30-81-191.ec2.internal>
2020-02-25 10:08:56 +0000

    Imported ConfigParser Module

--- python_for_SAP/connection.py
+++ python_for_SAP/connection.py
@@ -1,17 +1,22 @@
 from pyrfc import Connection
-import configparser
+
+try:
+    from ConfigParser import ConfigParser
+except ModuleNotFoundError as e:
+    from configparser import ConfigParser
 
 
 def connect():
 
     config = configparser.ConfigParser()
-    config.read("/root/SAP_System_Refresh/config.cnf")
+    config.read("/root/SAP_System_Refresh/python_for_SAP/config.cnf")
     creds = config['SAP']
-    
+
     conn = Connection(user=creds['user'], passwd=creds['passwd'], ashost=creds['ashost'], sysnr=creds['sysnr'], sid=creds['sid'], client=creds['client'])
-    
+
     attr = conn.get_connection_attributes()
     if attr['isoLanguage'] != u'EN':
         raise pyrfc.RFCError("Testing must be done with English language")
 
 connect()
+
