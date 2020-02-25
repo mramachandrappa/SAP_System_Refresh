406333268bddf83ddcc49199a9e0a3279a241605
root <root@ip-172-30-81-191.ec2.internal>
2020-02-25 14:01:43 +0000

    Script to fetch users list from SAP system

--- python_for_SAP/read_table.py
+++ python_for_SAP/read_table.py
@@ -9,17 +9,28 @@ def connect():
     creds = config['SAP']
 
     conn = Connection(user=creds['user'], passwd=creds['passwd'], ashost=creds['ashost'], sysnr=creds['sysnr'], sid=creds['sid'], client=creds['client'])
+    
+   # conn.call("BAPI_USER_UNLOCK", USERNAME='SAP*')
+
 
     tables = conn.call("RFC_GET_TABLE_ENTRIES", TABLE_NAME='USR02')
+    
+    user_names = []
 
-    print(tables)
+    for key, value in tables.items():
+        print(key, '->', value)
+        if key == 'ENTRIES':
+            entries = value
+            for users in entries:
+                for name in users.values():
+                    user_names.append(name[3:])
 
-    fields= []
-    fields_name = []
-
-    data_fields = tables["DATA"]
-    data_names = tables["FIELDS"]
-
-
+    #for users in user_names:
+    #3    if users == 'BJOERN':
+      #      pass
+       # else:
+        #    conn.call("BAPI_USER_LOCK", USERNAME=users)
+    
+    print(user_names)
 
 connect()
