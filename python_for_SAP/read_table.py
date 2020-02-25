a0d022f205782f9c32014b0890a94cc1f29e4321
Mahesh Ramachandrappa <mramachandrappa@sst.local>
2020-02-25 19:39:34 +0530

    Implemented class for script

--- python_for_SAP/read_table.py
+++ python_for_SAP/read_table.py
@@ -2,28 +2,27 @@ from pyrfc import Connection
 from configparser import ConfigParser
 
 
-def connect():
+class main():
 
-    config = ConfigParser()
-    config.read("/root/SAP_System_Refresh/python_for_SAP/config.cnf")
-    creds = config['SAP']
+    def __init__(self):
+        config = ConfigParser()
+        config.read("/root/SAP_System_Refresh/python_for_SAP/config.cnf")
+        creds = config['SAP']
 
-    conn = Connection(user=creds['user'], passwd=creds['passwd'], ashost=creds['ashost'], sysnr=creds['sysnr'], sid=creds['sid'], client=creds['client'])
+        self.conn = Connection(user=creds['user'], passwd=creds['passwd'], ashost=creds['ashost'], sysnr=creds['sysnr'], sid=creds['sid'], client=creds['client'])
     
-   # conn.call("BAPI_USER_UNLOCK", USERNAME='SAP*')
-
-
-    tables = conn.call("RFC_GET_TABLE_ENTRIES", TABLE_NAME='USR02')
+    def users_list(self):
+        tables = self.conn.call("RFC_GET_TABLE_ENTRIES", TABLE_NAME='USR02')
     
-    user_names = []
+        user_names = []
 
-    for key, value in tables.items():
-        print(key, '->', value)
-        if key == 'ENTRIES':
-            entries = value
-            for users in entries:
-                for name in users.values():
-                    user_names.append(name[3:])
+        for key, value in tables.items():
+            print(key, '->', value)
+            if key == 'ENTRIES':
+                entries = value
+                for users in entries:
+                    for name in users.values():
+                        user_names.append(name[3:])
 
     #for users in user_names:
     #3    if users == 'BJOERN':
@@ -31,6 +30,7 @@ def connect():
        # else:
         #    conn.call("BAPI_USER_LOCK", USERNAME=users)
     
-    print(user_names)
+        print(user_names)
 
-connect()
+
+main()
