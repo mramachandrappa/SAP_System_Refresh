e3b70cc3ccbe163544fedc9228be370c5473757c
Mahesh Ramachandrappa <mramachandrappa@sst.local>
2020-02-27 19:11:32 +0530

    Added suspend_jobs() & export_sys_tables() methods

--- python_for_SAP/read_table.py
+++ python_for_SAP/read_table.py
@@ -9,8 +9,7 @@ class SAPRefresh:
         self.config.read("/root/SAP_System_Refresh/python_for_SAP/config.cnf")
         self.creds = self.config['SAP']
 
-        self.conn = Connection(user=self.creds['user'], passwd=self.creds['passwd'], ashost=self.creds['ashost'],
-                               sysnr=self.creds['sysnr'], sid=self.creds['sid'], client=self.creds['client'])
+        self.conn = Connection(user=self.creds['user'], passwd=self.creds['passwd'], ashost=self.creds['ashost'], sysnr=self.creds['sysnr'], sid=self.creds['sid'], client=self.creds['client'])
     
     def users_list(self, table_name):
         tables = self.conn.call("RFC_GET_TABLE_ENTRIES", TABLE_NAME=table_name)
@@ -27,8 +26,7 @@ class SAPRefresh:
         return user_names
 
     def locked_users(self):
-        tables = self.conn.call("BAPI_USER_GETLIST", SELECTION_RANGE=[{'PARAMETER': 'ISLOCKED', 'FIELD': 'LOCAL_LOCK',
-                                                                       'SIGN': 'I', 'OPTION': 'EQ', 'LOW': 'L'}])
+        tables = self.conn.call("BAPI_USER_GETLIST", SELECTION_RANGE=[{'PARAMETER': 'ISLOCKED', 'FIELD': 'LOCAL_LOCK', 'SIGN': 'I', 'OPTION': 'EQ', 'LOW': 'L'}])
         locked_user_list = []
 
         for key, value in tables.items():
@@ -46,29 +44,34 @@ class SAPRefresh:
     def user_lock(self, user_list):
         f = open("except_user_list.txt", "r")
         except_users_list = f.read()
-        
+
         print("Except_users_list =>", except_users_list)
 
         users_locked = []
 
-#        for user in user_list:
- #           if user not in except_users_list:
-  #              self.conn.call('BAPI_USER_LOCK', USERNAME=user)
-   #             users_locked.append(user)
+        for user in user_list:
+            if user not in except_users_list:
+                self.conn.call('BAPI_USER_LOCK', USERNAME=user)
+                users_locked.append(user)
 
         return users_locked
-        
 
-    
+    def suspend_jobs(self, program_value):
+
+        print(self.conn.call("INST_EXECUTE_REPORT", PROGRAM=program_value))
+
+    def export_sys_tables(self, cmd):
+
+        print(self.conn.call("SXPG_COMMAND_EXECUTE", COMMANDNAME=cmd))
 
 s = SAPRefresh()
-user_list = s.users_list('USR02')
-locked_users = s.locked_users()
-users_locked = s.user_lock(user_list)
+#user_list = s.users_list('USR02')
+#locked_users = s.locked_users()
+#users_locked = s.user_lock(user_list)
 
-print("User_list =>", user_list)
-print("Already_Locked_users =>", locked_users)
-print("Final_users_locked =>", users_locked)
+#print("User_list =>", user_list)
+#print("Already_Locked_users =>", locked_users)
+#print("Final_users_locked =>", users_locked)
 
-
-
+#s.suspend_jobs('BTCTRNS1')
+#s.export_sys_tables('ZTABEXP')
