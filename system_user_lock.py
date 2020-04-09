178b0c9592b244e4b5dc3b3feb101fe51d261256
Mahesh Ramachandrappa <mramachandrappa@sst.local>
2020-04-09 18:34:57 +0530

    Added while condition for user input

--- system_user_lock.py
+++ system_user_lock.py
@@ -8,63 +8,74 @@ def main():
 
     # --------------------Step: 1 > Fetch list of users from USR02 table----------
 
-    option = input("Step: 1 > Fetch list of users from USR02 table [proceed | cancel]:\n")
+    option = input("\nStep: 1 > Fetch list of users from USR02 table [proceed | cancel]: ")
 
     users_list = None
     while True:
         if option == "proceed":
             users_list = lock.users_list()
-            print("List of users from USR02 table =>", users_list, "\n")
+            print("\nList of users from USR02 table =>", users_list)
             break
         elif option == "cancel":
             break
         else:
-            option = input("Please check the option specified [proceed | cancel]:\n")
+            option = input("\nPlease check the option specified [proceed | cancel]: ")
+            continue
 
     # ------------------- Step: 2 > list of users who's status is already set to administrator lock---------------
 
-    option = input("Step: 2 > Fetch users who's status is already set to administrator lock [proceed | cancel]:\n")
+    option = input("\nStep: 2 > Fetch users who's status is already set to administrator lock [proceed | cancel]: ")
 
     locked_users = None
-    if option == "proceed":
-        locked_users = lock.locked_users()
-        print("List of users whose status is already set to administrator lock  =>", locked_users)
-    elif option == "cancel":
-        pass
-    else:
-        print("Please check the option specified [proceed | cancel]")
+    while True:
+        if option == "proceed":
+            locked_users = lock.locked_users()
+            print("\nList of users whose status is already set to administrator lock  =>", locked_users)
+            break
+        elif option == "cancel":
+            break
+        else:
+            option = input("\nPlease check the option specified [proceed | cancel]: ")
+            continue
 
     # -------------------- Step: 3 > Enter the Exception user list provided by customer-------------------------
 
-    option = input("Step: 3 > Pass Exception user list: [proceed | cancel]\n")
+    option = input("\nStep: 3 > Pass Exception user list to kept unlocked from administer locking [proceed | cancel]: ")
+
+    user = None
     if option == "proceed":
-        user = input("Step: 3 > Enter the Exception user list to kept unlocked. Once done enter [Done] :\n")
+        user = input("Enter the list of users one by one and type [Done] once completed: \n")
 
-        my_list = [user]
-        while True:
-            user = input()
-            if user == "Done":
-                break
-            else:
-                my_list.append(user)
-
-        print("Exception user list entered are =>", my_list)
-    elif option == "cancel":
-        pass
-    else:
-        print("Please check the option specified [proceed | cancel]")
+    while True:
+        if option == "proceed":
+            my_list = [user]
+            while True:
+                user = input()
+                if user == "Done":
+                    break
+                else:
+                    my_list.append(user)
+            print("\nException user list entered are =>", my_list)
+        elif option == "cancel":
+            break
+        else:
+            option = input("\nPlease check the option specified [proceed | cancel]: ")
+            continue
 
     # --------------------- Step: 4 > Lock all users except the list of users obtained from customer-------------
 
-    option = input("Proceed to lock the users with exception to the user list provided: [proceed | cancel]\n")
+    option = input("\n Step: 4 > lock all the users before starting quality refresh [proceed | cancel]: ")
 
-    if option == "proceed":
-        users_locked = lock.user_lock(users_list, locked_users)
-        print("Locked user's list =>", users_locked)
-    elif option == "cancel":
-        pass
-    else:
-        print("Please check the option specified [proceed | cancel]")
+    while True:
+        if option == "proceed":
+            users_locked = lock.user_lock(users_list, locked_users)
+            print("Locked user's list =>", users_locked)
+            break
+        elif option == "cancel":
+            break
+        else:
+            option = input("Please check the option specified [proceed | cancel]: ")
+            continue
 
 
 if __name__ == '__main__':
