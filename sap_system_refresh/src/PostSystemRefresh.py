f5568baf37407224140602966221e341905aeb1f
Mahesh Ramachandrappa <mramachandrappa@sst.local>
2020-04-16 21:36:44 +0530

    Updated return statements

--- sap_system_refresh/src/PostSystemRefresh.py
+++ sap_system_refresh/src/PostSystemRefresh.py
@@ -2,7 +2,7 @@
 from pyrfc import Connection
 from configparser import ConfigParser
 import os
-
+import sap_system_refresh.src.PreSystemRefresh
 
 class PostSystemRefresh:
 
@@ -13,16 +13,28 @@ class PostSystemRefresh:
 
         self.conn = Connection(user=self.creds['user'], passwd=self.creds['passwd'], ashost=self.creds['ashost'], sysnr=self.creds['sysnr'], sid=self.creds['sid'], client=self.creds['client'])
 
+        self.preRefresh = PostSystemRefresh()
+
+    def prRed(self, text):
+        return "\033[91m {}\033[00m".format(text)
+
+    def prGreen(self, text):
+        return "\033[92m {}\033[00m".format(text)
+
+    def prYellow(self, text):
+        print("\033[93m {}\033[00m".format(text))
+
     def users_list(self):
-        tables = self.conn.call("RFC_READ_TABLE", QUERY_TABLE='USR02', FIELDS=[{'FIELDNAME': 'BNAME'}])
+        try:
+            tables = self.conn.call("RFC_READ_TABLE", QUERY_TABLE='USR02', FIELDS=[{'FIELDNAME': 'BNAME'}])
+        except Exception as e:
+            return self.prRed("\nError while fetching user's list from USR02 table: {}".format(e))
 
         users = []
-        try:
-            for data in tables['DATA']:
-                for names in data.values():
-                    users.append(names)
-        except Exception:
-            return "Error while fetching user's list"
+
+        for data in tables['DATA']:
+            for names in data.values():
+                users.append(names)
 
         return users
 
@@ -34,69 +46,63 @@ class PostSystemRefresh:
                     OPTION='EQ',
                     LOW='L'
                     )
-        tables = self.conn.call("BAPI_USER_GETLIST", SELECTION_RANGE=[params])
+        try:
+            user_list = self.conn.call("BAPI_USER_GETLIST", SELECTION_RANGE=[params])
+        except Exception as e:
+            return self.prRed("\nFailed to get already locked user list: {}".format(e))
+
         locked_user_list = []
 
-        try:
-            for data in tables['USERLIST']:
-                locked_user_list.append(data['USERNAME'])
-        except Exception:
-            return "Error while fetching locked user's list"
-
-        f = open("post_locked_user_list.txt", "w")
-        f.write(str(locked_user_list))
-        f.close()
+        for user in user_list['USERLIST']:
+                locked_user_list.append(user['USERNAME'])
 
         return locked_user_list
 
-    def user_lock(self, user_list):
-        f = open("except_user_list.txt", "r")
-        except_users_list = f.read()
-
-        print("Except_users_list =>", except_users_list)
-
+    def user_lock(self, user_list, except_users_list):
         users_locked = []
-        exceptions = []
 
         for user in user_list:
             if user not in except_users_list:
                 try:
                     self.conn.call('BAPI_USER_LOCK', USERNAME=user)
+                    self.prGreen("\nUser: " + user + " is locked!")
                     users_locked.append(user)
                 except Exception as e:
-                    exceptions.append(e)
+                    self.prRed("\nNot able to Lock user: " + user + "Please check! {}".format(e))
                     pass
+            else:
+                self.prYellow("\nUser: " + user + " is excepted from setting to Administer Lock.")
 
         return users_locked
 
     def suspend_jobs(self):
         try:
             self.conn.call("INST_EXECUTE_REPORT", PROGRAM='BTCTRNS1')
-            return "Suspended Background Jobs"
-        except Exception:
-            return "Error while suspending the Jobs"
+            return self.prGreen("\nBackground Jobs are suspended!")
+        except Exception as e:
+            return self.prRed("\nFailed to Suspend Background Jobs: {}".format(e))
 
     def check_background_jobs(self):
         try:
             output = self.conn.call("TH_WPINFO")
         except Exception as e:
-            return e
+            return self.prRed("\nError while call Function Module: {}".format(e))
 
         wp_type = []
         for type in output['WPLIST']:
             wp_type.append(type['WP_TYP'])
 
         if 'BGD' in wp_type:
-            return "“Background work process is not set to 0. Please change it immediately"
+            return self.prRed("\nBackground work process is not set to 0. Please change it immediately")
         else:
-            return "Background work process is set to 0. Proceeding with next step"
+            return self.prGreen("\nBackground work process is set to 0. Proceeding with next step")
 
     def import_sys_tables(self):
         try:
             self.conn.call("SXPG_COMMAND_EXECUTE", COMMANDNAME='ZTABIMP')
-            return "Successfully Imported Quality System Tables"
-        except Exception:
-            return "Error while exporting system tables"
+            return self.prGreen("\nSuccessfully Imported Quality System Tables")
+        except Exception as e:
+            return self.prRed("\nError while exporting system tables: {}".format(e))
 
     def check_variant(self, report, variant_name):
         try:
@@ -144,7 +150,10 @@ class PostSystemRefresh:
         if self.check_variant(report, variant_name) is False:
             return "Variant Successfully Deleted"
 
-    def del_old_bg_jobs(self, report, variant_name):
+    def del_old_bg_jobs(self):
+        report = "RSBTCDEL"
+        variant_name = "ZDELLOG"
+
         desc = dict(
             MANDT=self.creds['client'],
             REPORT=report,
@@ -167,25 +176,23 @@ class PostSystemRefresh:
 
         screen = [{'DYNNR': '1000', 'KIND': 'P'}]
 
-        variant = None
-
         if self.check_variant(report, variant_name) is False:
             try:
                 self.create_variant(report, variant_name, desc, content, text, screen)
-                variant = True
             except Exception as e:
-                return e
+                return self.prRed("Failed to create variant {}: {}".format(variant_name, e))
 
-        if variant is True:
+        if self.check_variant(report, variant_name) is True:
             try:
                 self.conn.call("SUBST_START_REPORT_IN_BATCH", IV_JOBNAME=report, IV_REPNAME=report, IV_VARNAME=variant_name)
-                return "Old Background jobs logs are successfully deleted."
+                return self.prGreen("\nOld Background jobs logs are successfully deleted.")
             except Exception as e:
-                return e
+                return self.prRed("\nFailed to delete Old Background job logs: {}".format(e))
         else:
-            return "Please check if variant exist"
+            return self.prRed("\nCreation of variant {} is failed!".format(variant_name))
 
-    def del_outbound_queues(self, jobname, report, variant_name):
+    def del_outbound_queues(self, jobname, report, variant_name): #For SMQ1 and SMQ2
+
         desc = dict(
             MANDT=self.creds['client'],
             REPORT=report,
@@ -200,25 +207,26 @@ class PostSystemRefresh:
 
         screen = [{'DYNNR': '1000', 'KIND': 'P'}]
 
-        variant = None
-
         if self.check_variant(report, variant_name) is False:
             try:
                 self.create_variant(report, variant_name, desc, content, text, screen)
-                variant = True
             except Exception as e:
-                return e
+                return self.prRed("\nFailed to create variant {}: {}".format(variant_name, e))
 
-        if variant is True:
+        if self.check_variant(report, variant_name) is True:
             try:
                 self.conn.call("SUBST_START_REPORT_IN_BATCH", IV_JOBNAME=jobname, IV_REPNAME=report, IV_VARNAME=variant_name)
-                return "Deleted all outbound Queues"
+                return self.prGreen("\nSDeleted all Outbound Queues Successfully!")
             except Exception as e:
-                return e
+                return self.prRed("Failed to delete Outbound Queues: {}".format(e))
         else:
-            return "Please check if variant exist"
+            return self.prRed("\nFailed to create variant {}".format(variant_name))
 
-    def del_trfc_queues_sm58(self, jobname, report, variant_name):
+    def del_trfc_queues_sm58(self):
+        jobname = "DELTE_SM58_QUEUES"
+        report = "RSARFCDL"
+        variant_name= "ZDELSM58"
+
         desc = dict(
             MANDT=self.creds['client'],
             REPORT=report,
@@ -228,24 +236,23 @@ class PostSystemRefresh:
         content = [{'SELNAME': 'TID', 'KIND': 'P', 'LOW': '*'},
                    {'SELNAME': 'SET_EXEC', 'KIND': 'P', 'LOW': 'X'}]
 
-        text = [{'MANDT': self.creds['client'], 'LANGU': 'EN', 'REPORT': report, 'VARIANT':variant_name, 'VTEXT': 'Delete all trfc Queues'}]
+        text = [{'MANDT': self.creds['client'], 'LANGU': 'EN', 'REPORT': report, 'VARIANT':variant_name, 'VTEXT': 'Delete all TRFC Queues'}]
 
         screen = [{'DYNNR': '1000', 'KIND': 'P'}]
-
-        variant = None
 
         if self.check_variant(report, variant_name) is False:
             try:
                 self.create_variant(report, variant_name, desc, content, text, screen)
-                variant = True
             except Exception as e:
-                return e
+                return self.prRed("\nFailed to create variant {}: {}".format(variant_name, e))
 
-        if variant is True:
+        if self.check_variant(report, variant_name) is True:
             try:
                 self.conn.call("SUBST_START_REPORT_IN_BATCH", IV_JOBNAME=jobname, IV_REPNAME=report, IV_VARNAME=variant_name)
-                return "Deleted all TRC Queues SM58"
+                return self.prGreen("\nDeleted all TRC Queues SM58")
             except Exception as e:
-                return e
+                return self.prRed("\nFailed to delete Outbound Queues: {}".format(e))
         else:
-            return "Please check if variant exist"
+            return self.prRed("\nFailed to create variant {}".format(variant_name))
+
+
