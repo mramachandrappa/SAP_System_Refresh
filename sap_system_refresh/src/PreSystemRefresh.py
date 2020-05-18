78cc008d6a03b5b01772833ab9584c2bcbb8d49e
Mahesh Ramachandrappa <mramachandrappa@sst.local>
2020-05-18 13:10:47 +0530

    Seperated user_master_export() to 2 functions

--- sap_system_refresh/src/PreSystemRefresh.py
+++ sap_system_refresh/src/PreSystemRefresh.py
@@ -204,29 +204,6 @@ class PreSystemRefresh:
 
     def user_master_export(self, pc3_val, ctc_val):
 
-        report = "ZRSCLXCOP"
-        variant_name = "SST_ZUSR_EXP"
-
-        desc = dict(
-            MANDT=self.creds['client'],
-            REPORT=report,
-            VARIANT=variant_name
-        )
-
-        content = [{'SELNAME': 'COPYCLI', 'KIND': 'P', 'LOW': self.creds['client']},
-                   {'SELNAME': 'SUSR', 'KIND': 'P', 'LOW': 'X'},
-                   {'SELNAME': 'MODUS', 'KIND': 'P', 'LOW': 'E'},
-                   {'SELNAME': 'ALTINP', 'KIND': 'P', 'LOW': 'A'},
-                   {'SELNAME': 'COMFILE', 'KIND': 'P', 'LOW': pc3_val},
-                   {'SELNAME': 'PROF', 'KIND': 'P', 'LOW': 'X'},
-                   {'SELNAME': 'PROFIL', 'KIND': 'P', 'LOW': 'SAP_USER'},
-                   {'SELNAME': 'TARGET', 'KIND': 'P', 'LOW': ctc_val}]
-
-        text = [{'MANDT': self.creds['client'], 'LANGU': 'EN', 'REPORT': report, 'VARIANT': variant_name,
-                 'VTEXT': 'User Master Export'}]
-
-        screen = [{'DYNNR': '1000', 'KIND': 'P'}]
-
         if pc3_val is not None and self.check_variant(report, variant_name) is False:
             try:
                 self.create_variant(report, variant_name, desc, content, text, screen)
